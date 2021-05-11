a7679a673df9213ea873a1874b1609615cdb1dbe
Carolinacapote <carolina.capote28@gmail.com>
2021-05-11 22:26:00 +0000

    Main file

---
+++ main_monty.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ *
+ *
+ */
+int main(int argc, char **argv)
+{
+	char *monty_file = argv[1];
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file");
+		exit(EXIT_FAILURE);
+	}
+	if (argc == 2)
+	{
+		/*abrir archivo*/
+	}
+}
