5365ea24ae467f9fe4465d25828519b74fbe2195
Carolinacapote <carolina.capote28@gmail.com>
2021-05-11 23:33:33 +0000

    Functions to be used

---
+++ opcode_functions.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+/**
+ *
+ */
+void push_function(stack_t **stack, unsigned int line_number)
+{
+}
+/**
+ *
+ *
+ */
+void pall_function(stack_t **stack, unsigned int line_number)
+{
+}
+/**
+ *
+ *
+ */
+void pint_function(stack_t **stack, unsigned int line_number)
+{
+}
+/**
+ *
+ *
+ */
+void pop_function(stack_t **stack, unsigned int line_number)
+{
+}
---
+++ opcode_more_functions.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ *
+ *
+ */
+void swap_function(stack_t **stack, unsigned int line_number)
+{
+}
+/**
+ *
+ *
+ */
+void add_function(stack_t **stack, unsigned int line_number)
+{
+}
+/**
+ *
+ */
+void nop_function(stack_t **stack, unsigned int line_number)
+{
+}
