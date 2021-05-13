ef39185dad5dab88f2ecc96dae5ca95c77e7f7e7
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-05-13 17:39:37 -0500

    [opcodeFunctions] update

--- opcode_advanced_functions.c
+++ opcode_advanced_functions.c
@@ -118,7 +118,7 @@ void pchar_function(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
